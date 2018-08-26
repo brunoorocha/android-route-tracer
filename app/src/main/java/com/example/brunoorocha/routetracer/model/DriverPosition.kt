74e035387d75793ab18627757c113df91dc5692a
Bruno Rocha <mail.brunorocha@gmail.com>
2018-08-26 13:15:18 -0300

    Refactored DriverPosition class to 'Kotlin-way' class

--- app/src/main/java/com/example/brunoorocha/routetracer/model/DriverPosition.kt
+++ app/src/main/java/com/example/brunoorocha/routetracer/model/DriverPosition.kt
@@ -1,14 +1,3 @@
 package com.example.brunoorocha.routetracer.model
 
-class DriverPosition {
-
-    var latitude: Double? = null
-    var longitude: Double? = null
-    var timestamp: Number? = null
-
-    constructor(latitude: Double, longitude: Double, timestamp: Number) {
-        this.latitude = latitude
-        this.longitude = longitude
-        this.timestamp = timestamp
-    }
-}
+data class DriverPosition(var latitude: Double, var longitude: Double, var timestamp: Number)
